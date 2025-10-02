43c881f07a1f586af1f8b0140494c38903fe61ea
Suzal Regmi <suzalregmii@gmail.com>
2025-10-01 23:14:58 -0400

    Implement Catpurrin-themed Flutter animation app with fading text, day/night toggle, swipe navigation, color picker, image frame toggle, and rotating animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// This is a basic Flutter widget test.
+// Collaborators: Suzal Regmi, Diana Avila
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,116 +10,466 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Animation App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const FadingTextAnimation(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FadingTextAnimation extends StatefulWidget {
+  const FadingTextAnimation({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<FadingTextAnimation> createState() => _FadingTextAnimationState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FadingTextAnimationState extends State<FadingTextAnimation>
+    with TickerProviderStateMixin {
+  bool _isVisible = true;
+  bool _isDarkMode = false;
+  Color _textColor = Colors.purple;
+  bool _showFrame = false;
+  late AnimationController _rotationController;
+  late AnimationController _fadeController;
+  int _currentScreen = 0;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _rotationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _fadeController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _rotationController.dispose();
+    _fadeController.dispose();
+    super.dispose();
+  }
+
+  void toggleVisibility() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isVisible = !_isVisible;
+    });
+  }
+
+  void toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
+  void _showColorPicker() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Choose Text Color'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildColorOption(Colors.purple, 'Purple'),
+                _buildColorOption(Colors.pink, 'Pink'),
+                _buildColorOption(Colors.blue, 'Blue'),
+                _buildColorOption(Colors.green, 'Green'),
+                _buildColorOption(Colors.orange, 'Orange'),
+                _buildColorOption(Colors.red, 'Red'),
+                _buildColorOption(Colors.teal, 'Teal'),
+                _buildColorOption(Colors.indigo, 'Indigo'),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildColorOption(Color color, String name) {
+    return ListTile(
+      leading: CircleAvatar(backgroundColor: color),
+      title: Text(name),
+      onTap: () {
+        setState(() {
+          _textColor = color;
+        });
+        Navigator.of(context).pop();
+      },
+    );
+  }
+
+  void _nextScreen() {
+    setState(() {
+      _currentScreen = (_currentScreen + 1) % 2;
+    });
+  }
+
+  void _previousScreen() {
+    setState(() {
+      _currentScreen = (_currentScreen - 1 + 2) % 2;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return Theme(
+      data: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Animation App'),
+          backgroundColor: _isDarkMode ? Colors.purple[900] : Colors.purple[400],
+          foregroundColor: Colors.white,
+          actions: [
+            IconButton(
+              icon: Icon(_isDarkMode ? Icons.light_mode : Icons.dark_mode),
+              onPressed: toggleTheme,
+              tooltip: 'Toggle Day/Night Mode',
+            ),
+            IconButton(
+              icon: const Icon(Icons.palette),
+              onPressed: _showColorPicker,
+              tooltip: 'Change Text Color',
+            ),
+          ],
+        ),
+        body: GestureDetector(
+          onHorizontalDragEnd: (details) {
+            if (details.primaryVelocity! > 0) {
+              _previousScreen();
+            } else if (details.primaryVelocity! < 0) {
+              _nextScreen();
+            }
+          },
+          child: PageView(
+            controller: PageController(initialPage: _currentScreen),
+            onPageChanged: (index) {
+              setState(() {
+                _currentScreen = index;
+              });
+            },
+            children: [
+              _buildFirstScreen(),
+              _buildSecondScreen(),
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                if (_rotationController.isAnimating) {
+                  _rotationController.stop();
+                } else {
+                  _rotationController.repeat();
+                }
+              },
+              heroTag: "rotation",
+              tooltip: 'Rotate Image',
+              child: const Icon(Icons.rotate_right),
+            ),
+            const SizedBox(height: 10),
+            FloatingActionButton(
+              onPressed: toggleVisibility,
+              heroTag: "fade",
+              tooltip: 'Toggle Fade',
+              child: const Icon(Icons.play_arrow),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentScreen,
+          onTap: (index) {
+            setState(() {
+              _currentScreen = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Screen 1',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: 'Screen 2',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildFirstScreen() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: _isDarkMode
+              ? [Colors.purple[900]!, Colors.pink[900]!]
+              : [Colors.purple[100]!, Colors.pink[100]!],
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Fading Text Animation
+            GestureDetector(
+              onTap: toggleVisibility,
+              child: AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeInOut,
+                child: Text(
+                  '🐱 Purr-fect Flutter! 🐱',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: _textColor,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withValues(alpha: 0.3),
+                        offset: const Offset(2, 2),
+                        blurRadius: 4,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            
+            // Image with frame toggle
+            Column(
+              children: [
+                Container(
+                  decoration: _showFrame
+                      ? BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                            color: _textColor,
+                            width: 4,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: _textColor.withValues(alpha: 0.3),
+                              blurRadius: 10,
+                              spreadRadius: 2,
+                            ),
+                          ],
+                        )
+                      : null,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(_showFrame ? 16 : 20),
+                    child: Image.asset(
+                      'assets/cat_image.png',
+                      width: 200,
+                      height: 200,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Container(
+                          width: 200,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            color: _textColor.withValues(alpha: 0.3),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Icon(
+                            Icons.pets,
+                            size: 100,
+                            color: Colors.white,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Show Frame: '),
+                    Switch(
+                      value: _showFrame,
+                      onChanged: (bool value) {
+                        setState(() {
+                          _showFrame = value;
+                        });
+                      },
+                      activeThumbColor: _textColor,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            
+            const SizedBox(height: 20),
+            Text(
+              'Swipe left/right or use bottom navigation to switch screens',
+              style: TextStyle(
+                fontSize: 14,
+                color: _isDarkMode ? Colors.white70 : Colors.black54,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSecondScreen() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: _isDarkMode
+              ? [Colors.indigo[900]!, Colors.purple[900]!]
+              : [Colors.indigo[100]!, Colors.purple[100]!],
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Different fading animation with longer duration
+            GestureDetector(
+              onTap: toggleVisibility,
+              child: AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(milliseconds: 2000), // 2 seconds
+                curve: Curves.bounceInOut,
+                child: Text(
+                  '🌟 Meow-gical Animation! 🌟',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: _textColor,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withValues(alpha: 0.3),
+                        offset: const Offset(2, 2),
+                        blurRadius: 4,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            
+            // Rotating image
+            AnimatedBuilder(
+              animation: _rotationController,
+              builder: (context, child) {
+                return Transform.rotate(
+                  angle: _rotationController.value * 2.0 * 3.14159,
+                  child: Container(
+                    decoration: _showFrame
+                        ? BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: _textColor,
+                              width: 4,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: _textColor.withValues(alpha: 0.3),
+                                blurRadius: 10,
+                                spreadRadius: 2,
+                              ),
+                            ],
+                          )
+                        : null,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(_showFrame ? 16 : 20),
+                      child: Image.asset(
+                        'assets/cat_image.png',
+                        width: 180,
+                        height: 180,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            width: 180,
+                            height: 180,
+                            decoration: BoxDecoration(
+                              color: _textColor.withValues(alpha: 0.3),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Icon(
+                              Icons.pets,
+                              size: 80,
+                              color: Colors.white,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Show Frame: '),
+                Switch(
+                  value: _showFrame,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _showFrame = value;
+                    });
+                  },
+                  activeThumbColor: _textColor,
+                ),
+              ],
+            ),
+            
+            const SizedBox(height: 20),
+            Text(
+              'This screen has a slower, bouncy fade animation!',
+              style: TextStyle(
+                fontSize: 16,
+                color: _isDarkMode ? Colors.white70 : Colors.black54,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
